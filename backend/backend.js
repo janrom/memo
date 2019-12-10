9c4cc37edb0b799348763a21c80fdf57424e7e88
Janne Romppanen <janneromppanen@fastmail.com>
2019-12-10 17:23:08 +0200

    Refactored get notes route

--- backend/backend.js
+++ backend/backend.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
 const Note = require('./models/Note');
 
 const PORT = 3001;
@@ -9,6 +10,7 @@ const PORT = 3001;
 const app = express();
 app.use(cors());
 app.use(logger('dev'));
+app.use(bodyParser.json());
 
 mongoose.connect(process.env.MEMO_MONGODB, {
   useNewUrlParser: true,
@@ -20,12 +22,14 @@ db.on('open', () => console.log('Connected to database'));
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.get('/notes', (req, res) => {
-  Note.find((err, notes) => {
-    if (err) {
-      return res.json({ success: false, error: err });
-    }
-    return res.json({ success: true, notes });
-  });
+  Note.find()
+    .exec((err, notes) => {
+      if (err) {
+        return res.json({ err });
+      }
+      res.append('Content-Type', 'application/json');
+      return res.json({ notes });
+    });
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
