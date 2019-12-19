26cf82430e38757c5dd2eab8848cccd7e1fd3966
Janne Romppanen <janneromppanen@fastmail.com>
2019-12-19 16:05:46 +0200

    Added update and change handlers and refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import Note from './Note';
+import axios from 'axios';
 import AddNote from './AddNote';
 import ListNotes from './ListNotes';
+
+const BACKEND_URL = 'http://localhost:3001';
 
 class App extends React.Component {
   constructor(props) {
@@ -11,18 +13,29 @@ class App extends React.Component {
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
-    this.fetchNotes();
+    const { notes } = this.state;
+    if (notes.length === 0) {
+      this.getNotes();
+    }
   }
 
   componentDidUpdate(prevProps, prevState) {
     // event after setState is finished
+    const { notes } = this.state;
+    if (notes.length !== prevState.notes.length) {
+      console.log('this.state.notes: ', notes);
+      console.log('prevState.notes: ', prevState.notes);
+    }
   }
 
-  fetchNotes() {
-    fetch('http://localhost:3001/notes')
+  getNotes() {
+    console.log('Fetch notes');
+    fetch(`${BACKEND_URL}/notes`)
       .then((res) => res.json())
       .then((json) => {
         if (json.err) {
@@ -30,39 +43,102 @@ class App extends React.Component {
           // FIXME
           return alert('Fetching notes failed.');
         }
-        console.log(json.notes);
-        this.setState({ notes: json.notes });
+        return this.setState((state) => (
+          { notes: state.notes.concat(json.notes) }
+        ));
       })
       .catch((err) => console.log(err));
   }
 
   // Create new note from user input.
   handleSubmit(e) {
-    const note = Note({
+    e.preventDefault();
+
+    const note = {
       heading: e.target.heading.value,
       content: e.target.content.value,
+    };
+
+    e.target.heading.value = '';
+    e.target.content.value = '';
+
+    axios({
+      method: 'post',
+      url: `${BACKEND_URL}/add`,
+      data: note,
+    }).then((res) => {
+      this.setState((prevState) => (
+        { notes: prevState.notes.concat(res.data) }
+      ));
+    });
+  }
+
+  handleChange(e) {
+    let { notes } = this.state;
+    const changedNote = notes.find((n) => n._id === e.target.id);
+
+    if (!changedNote) return;
+
+    if (e.target.name === 'heading') {
+      changedNote.heading = e.target.value;
+    } else {
+      changedNote.content = e.target.value;
+    }
+
+    notes = notes.map((n) => {
+      if (n._id === changedNote.id) {
+        return changedNote;
+      }
+      return n;
     });
 
-    this.setState((state) => (
-      {
-        notes: state.notes.concat([note]),
-      }
-    ));
+    this.setState({ notes });
   }
+
+  handleUpdate(e) {
+    e.preventDefault();
+
+    const { notes } = this.state;
+    const changedNote = notes.find((n) => n._id === e.target.heading.id);
+
+    if (!changedNote) return;
+
+    axios({
+      method: 'put',
+      url: `${BACKEND_URL}/update`,
+      data: changedNote,
+    }).then((res) => {
+      console.log(res.data);
+    });
+  }
+
 
   // Delete the note which delete button is clicked
   handleDelete(e) {
-    const { notes } = this.state;
-    const filteredNotes = notes.filter((note) => note._id !== e.target.value);
+    let { notes } = this.state;
+    const deleteId = e.target.name;
+    notes = notes.filter((note) => note._id !== deleteId);
 
-    this.setState({ notes: filteredNotes });
+    this.setState({ notes });
+    axios({
+      method: 'delete',
+      url: `${BACKEND_URL}/delete`,
+      data: { deleteId },
+    }).then((res) => {
+      console.log(res.data);
+    });
   }
 
   render() {
     const { notes } = this.state;
     return (
       <>
-        <ListNotes notes={notes} handleDelete={this.handleDelete} />
+        <ListNotes
+          notes={notes}
+          handleChange={this.handleChange}
+          handleUpdate={this.handleUpdate}
+          handleDelete={this.handleDelete}
+        />
         <AddNote onSubmit={this.handleSubmit} />
       </>
     );
